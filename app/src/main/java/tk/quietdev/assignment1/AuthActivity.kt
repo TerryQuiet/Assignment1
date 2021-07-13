b06cd919f62e4045f742733586cc59b48fd992d2
QuietDev <quietdev@pm.me>
2021-07-13 20:13:07 +0300

    saving credentials implemented with sharedPreferences

--- app/src/main/java/tk/quietdev/assignment1/AuthActivity.kt
+++ app/src/main/java/tk/quietdev/assignment1/AuthActivity.kt
@@ -1,30 +1,53 @@
 package tk.quietdev.assignment1
 
+
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import tk.quietdev.assignment1.databinding.ActivityAuthBinding
 
+const val IS_REMEMBER = "isSaveChecked"
+const val EMAIL = "email"
+const val PASSWORD = "password"
+
+
+private const val TAG = "AuthActivity"
 
 class AuthActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAuthBinding
     private lateinit var db: DB
+    private lateinit var preferences : SharedPreferences
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.supportActionBar?.hide()
         db = DB()
         binding = ActivityAuthBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        preferences = getSharedPreferences(packageName, Context.MODE_PRIVATE)
+
+        setContentView(binding.root)
+
+        getCredentials()
 
         binding.btnRegister.setOnClickListener {
             login(it)
         }
+
+    }
+
+    private fun getCredentials() {
+        binding.etEmail.setText(preferences.getString(EMAIL,""))
+        binding.etPassword.setText(preferences.getString(PASSWORD,""))
+        val isChecked = preferences.getBoolean(IS_REMEMBER,false)
+        binding.checkBoxRemember.isChecked = isChecked
     }
 
     private fun login(view: View) {
@@ -40,6 +63,12 @@ class AuthActivity : AppCompatActivity() {
                 putExtra(User.PHYSICAL_ADDRESS, user.physicalAddress)
                 putExtra(User.PICTURE, user.picture)
             }
+
+            if (binding.checkBoxRemember.isChecked) {
+                saveCredentials()
+            } else {
+                clearCredentials()
+            }
             startActivity(intent)
             //finish() //should it be done like that?
         } else {
@@ -52,6 +81,19 @@ class AuthActivity : AppCompatActivity() {
                 .show()
         }
 
+    }
+
+    private fun clearCredentials() {
+        preferences.edit().clear().apply()
+    }
+
+    private fun saveCredentials() {
+        preferences.edit()
+            .putBoolean(IS_REMEMBER, binding.checkBoxRemember.isChecked)
+            .putString(EMAIL, binding.etEmail.text.toString())
+            .putString(PASSWORD, binding.etPassword.text.toString())
+            .apply()
+        Log.d(TAG, "saveCredentials: SAVED")
     }
 
 
