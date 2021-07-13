f5ddd2e61aca337d0c79a5e606cc97dad51d3865
QuietDev <quietdev@pm.me>
2021-07-13 15:27:11 +0300

    added picture to user
    Edited activity_auth.xml, is is better?

--- README.md
+++ README.md
@@ -3,12 +3,16 @@ I have no idea why it's not working with login/password
 it works with token tho
 
 added icons 
+
 added rounded imageView 
 https://howtodoandroid.com/shapeableimageview-material-components-android/
-
 
 ok, the plan was to write everything here but I failed xD
 
 turned off dark theme 
 https://stackoverflow.com/a/67064345/12972985
+
+
+
+
 
--- app/src/main/java/tk/quietdev/assignment1/MainActivity.kt
+++ app/src/main/java/tk/quietdev/assignment1/MainActivity.kt
@@ -16,8 +16,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun bindValues() {
-        binding.tvName.text = intent.getStringExtra(User.USERNAME)
-        binding.tvAddress.text = intent.getStringExtra(User.PHYSICAL_ADDRESS)
-        binding.tvOccupation.text = intent.getStringExtra(User.OCCUPATION)
+        binding.apply {
+            tvName.text = intent.getStringExtra(User.USERNAME)
+            tvAddress.text = intent.getStringExtra(User.PHYSICAL_ADDRESS)
+            tvOccupation.text = intent.getStringExtra(User.OCCUPATION)
+            ivProfilePic.setImageResource(intent.getIntExtra(User.PICTURE, R.drawable.ic_profile))
+        }
     }
 }
