b1025faa0e669327137d920f0bcecbce2c3bf840
QuietDev <quietdev@pm.me>
2021-07-13 15:12:25 +0300

    off dark theme.

--- app/src/main/java/tk/quietdev/assignment1/User.kt
+++ app/src/main/java/tk/quietdev/assignment1/User.kt
@@ -5,13 +5,20 @@ package tk.quietdev.assignment1
 data class User(
     var userName: String? = null,
     var email: String,
-    var occupation: String? = null,
-    var physicalAddress: String? = null,
+    var occupation: String = "Not provided",
+    var physicalAddress: String = "Not provided",
     var picture: Int = R.drawable.ic_profile,
     private var password: String
 ) {
     fun isPasswordCorrect(inputPassword: String): Boolean {
         return password == inputPassword
+    }
+
+    companion object {
+        const val USERNAME = "userName"
+        const val OCCUPATION = "occupation"
+        const val PHYSICAL_ADDRESS = "physicalAddress"
+        const val PICTURE = "picture"
     }
 }
 
