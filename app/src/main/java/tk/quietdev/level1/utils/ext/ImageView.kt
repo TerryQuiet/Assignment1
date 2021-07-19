8877b0cb13ae08ed335b398b99e46affebd99729
QuietDev <quietdev@pm.me>
2021-07-19 17:56:02 +0300

    glide implemented

---
+++ app/src/main/java/tk/quietdev/level1/utils/ext/ImageView.kt
@@ -0,0 +1,24 @@
+package tk.quietdev.level1.utils.ext
+
+
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+
+
+fun ImageView.loadImage(url: String) {
+    Glide.with(this)
+        .load(url)
+        .circleCrop()
+        .into(this)
+}
+
+
+fun ImageView.loadImage(resource: Int) {
+    Glide.with(this)
+        .load(resource)
+        .circleCrop()
+        .into(this)
+}
+
+
+
