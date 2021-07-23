fe35e414c76e5615d159d8d6d89bdd8dd731b6c7
QuietDev <quietdev@pm.me>
2021-07-23 22:05:33 +0300

    viewModel implemented, no idea if I did it right.

---
+++ app/src/main/java/tk/quietdev/level1/ui/contacts/ContactsViewModel.kt
@@ -0,0 +1,68 @@
+package tk.quietdev.level1.ui.contacts
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.RecyclerView
+import tk.quietdev.level1.database.FakeDatabase
+import tk.quietdev.level1.databinding.DialogAddContactBinding
+import tk.quietdev.level1.models.User
+
+import tk.quietdev.level1.utils.OnSwipeCallBack
+
+
+class ContactsViewModel : ViewModel(), OnSwipeCallBack.Listener {
+
+
+    private var deletedUserPosition = 0
+
+    val userList = MutableLiveData(FakeDatabase.userContacts)
+    val deletedUser = MutableLiveData("")
+
+
+    private fun updateLiveData() {
+        userList.value = FakeDatabase.userContacts
+    }
+
+
+    fun addUserBack() {
+        if (deletedUser.value.toString().isNotEmpty()) {
+            FakeDatabase.userContacts.add(deletedUserPosition, deletedUser.value.toString())
+            updateLiveData()
+            deletedUser.value = ""
+        }
+    }
+
+    private fun addNewUserToDatabase(user: User) {
+        FakeDatabase.allFakeUsers[user.email] = user
+        FakeDatabase.userContacts.add(user.email)
+        updateLiveData()
+    }
+
+    fun removeUser(email: String?) {
+        val position = FakeDatabase.userContacts.indexOf(email)
+        deletedUser.value = FakeDatabase.userContacts.removeAt(position)
+        deletedUserPosition = position
+        updateLiveData()
+    }
+
+    override fun swipedOn(viewHolder: RecyclerView.ViewHolder) {
+        val email = (viewHolder as RecycleViewAdapter.ItemViewHolder).email
+        removeUser(email)
+    }
+
+    fun onDialogAddClicked(dialogBinding: DialogAddContactBinding) {
+        val name = dialogBinding.etName.text.toString()
+        val surname = dialogBinding.etSurname.text.toString()
+        val occupation = dialogBinding.etOccupation.text.toString()
+
+        val user = User(
+            userName = "$name $surname",
+            email = "$name.$surname@mail.fake",
+            occupation = occupation
+        )
+        addNewUserToDatabase(user)
+    }
+
+
+
+}
