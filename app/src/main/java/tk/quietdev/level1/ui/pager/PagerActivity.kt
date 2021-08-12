0d98af34eb0278531f84ac4fa086a7d923a5ad86
QuietDev <quietdev@pm.me>
2021-08-13 01:37:14 +0300

    AFTER removing NavHostFragment, no idea if all still good

--- app/src/main/java/tk/quietdev/level1/ui/pager/PagerActivity.kt
+++ app/src/main/java/tk/quietdev/level1/ui/pager/PagerActivity.kt
@@ -1,22 +1,43 @@
 package tk.quietdev.level1.ui.pager
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.lifecycle.Observer
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import tk.quietdev.level1.R
-import tk.quietdev.level1.databinding.ActivityNavHostHolderBinding
+import tk.quietdev.level1.databinding.ActivityPagerBinding
+
 
 class PagerActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityNavHostHolderBinding
+    private lateinit var binding: ActivityPagerBinding
+    private var navController: NavController? = null
+    private val nestedNavHostFragmentId = R.id.ParentNavHost
+    private val appbarSharedViewModel: AppbarSharedViewModel by viewModel() // TODO: 8/13/2021 by inject?
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityNavHostHolderBinding.inflate(layoutInflater)
+        binding = ActivityPagerBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        val nestedNavHostFragment =
+            supportFragmentManager.findFragmentById(nestedNavHostFragmentId) as? NavHostFragment
+        navController = nestedNavHostFragment?.navController
+
+        val appBarConfig = AppBarConfiguration(navController!!.graph)
+        binding!!.toolbar.setupWithNavController(navController!!, appBarConfig)
+
+
+        appbarSharedViewModel.currentNavController.observe(this, Observer { navController ->
+            navController?.let {
+                val appBarConfig = AppBarConfiguration(it.graph)
+                binding!!.toolbar.setupWithNavController(it, appBarConfig)
+            }
+        })
     }
 
 }
