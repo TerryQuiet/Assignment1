ba1f8669ca98b063c04be0f4f7c5dd8927e480a5
QuietDev <quietdev@pm.me>
2021-08-30 01:02:07 +0300

    removed weird code, i don't know what it did xD

--- app/src/main/java/tk/quietdev/level1/ui/pager/PagerAdapter.kt
+++ app/src/main/java/tk/quietdev/level1/ui/pager/PagerAdapter.kt
@@ -13,28 +13,6 @@ import tk.quietdev.level1.ui.pager.settings.SettingsFragment
 class PagerAdapter(fm: FragmentManager, lf: Lifecycle) :
         FragmentStateAdapter(fm, lf) {
 
-    init {
-        // Add a FragmentTransactionCallback to handle changing
-        // the primary navigation fragment
-        registerFragmentTransactionCallback(object : FragmentTransactionCallback() {
-            override fun onFragmentMaxLifecyclePreUpdated(
-                fragment: Fragment,
-                maxLifecycleState: Lifecycle.State
-            ) = if (maxLifecycleState == Lifecycle.State.RESUMED) {
-
-                // This fragment is becoming the active Fragment - set it to
-                // the primary navigation fragment in the OnPostEventListener
-                OnPostEventListener {
-                    fragment.parentFragmentManager.commitNow {
-                        setPrimaryNavigationFragment(fragment)
-                    }
-                }
-            } else {
-                super.onFragmentMaxLifecyclePreUpdated(fragment, maxLifecycleState)
-            }
-        })
-    }
-
         override fun getItemCount(): Int = 2
 
         override fun createFragment(position: Int): Fragment {
