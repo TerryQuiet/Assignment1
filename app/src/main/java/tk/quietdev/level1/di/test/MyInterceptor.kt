6be7f4d3ab72fb4d235e3c958aa74ca39decdca4
QuietDev <quietdev@pm.me>
2021-09-14 00:50:26 +0300

    IMMEGAPROGRAMMER fixed missing closing bracket

--- app/src/main/java/tk/quietdev/level1/di/test/MyInterceptor.kt
+++ app/src/main/java/tk/quietdev/level1/di/test/MyInterceptor.kt
@@ -1,17 +1,12 @@
 package tk.quietdev.level1.di.test
 
 import android.util.Log
-import com.google.gson.JsonObject
-
 import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
-import okhttp3.ResponseBody
 import okhttp3.ResponseBody.Companion.toResponseBody
-
 import okio.Buffer
 import tk.quietdev.level1.utils.ext.fixJson
-
 import java.nio.charset.Charset
 
 
@@ -24,9 +19,9 @@ class MyInterceptor : Interceptor {
             it.request(Long.MAX_VALUE)
             val buffer: Buffer = source.buffer
             val responseBodyString: String = buffer.clone().readString(Charset.forName("UTF-8"))
-            Log.d("TAG", "intercept pre fix: $responseBodyString")
+            Log.d("TAG", "intercept pre fix     : $responseBodyString")
             val fixed = responseBodyString.fixJson()
-            Log.d("TAG", "intercept after fix: $fixed")
+            Log.d("TAG", "intercept after fix   : $fixed")
             val contentType = response.body?.contentType()
             val body = fixed.toResponseBody(contentType)
             return response.newBuilder().body(body).build()
