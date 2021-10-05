0ed69e9c7078f944e632e350102f2d2b5c10bae8
Taras <811171@gmail.com>
2021-10-05 16:27:27 +0300

    ViewPager view model refactor

--- app/src/main/java/tk/quietdev/level1/ui/main/myprofile/contacts/pager/PagerAdapter.kt
+++ app/src/main/java/tk/quietdev/level1/ui/main/myprofile/contacts/pager/PagerAdapter.kt
@@ -1,7 +1,6 @@
 package tk.quietdev.level1.ui.main.myprofile.contacts.pager
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
